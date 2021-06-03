0e38ac4e36c5b02e676a7539b07e28d928dab9e3
attila <attilavigh.contact@gmail.com>
2021-06-03 15:34:38 +0100

    Function addNewPlayerInList() added.
    The user should be able to add a list of names, find a way to get the user input and fill a list with the new name.

--- script.js
+++ script.js
@@ -2,3 +2,47 @@
 // testing background
 document.querySelector("body").style.backgroundColor = "#cdecf1";
 
+const addPayerName = function () {
+    // .value
+    
+    
+    newPlayer.addEventListener("change", () => console.log(newPlayer.value) )
+    
+    return newPlayer.value
+}
+
+
+const addNewPlayerInList = function ( ) {
+    const newPlayer = document.querySelector("#player")
+    const allPlayersNameList = document.querySelector("#allPlayersNameList")
+    const buttonAddPlayer = document.querySelector("#button-addPlayer")
+    buttonAddPlayer.addEventListener("click", () => {
+        let playerList = []
+        playerList.push(newPlayer.value)
+        playerList.forEach( player => 
+            allPlayersNameList.innerHTML 
+                += `
+                <li class="list-group-item">${player}</li>
+                `
+        )
+    })
+
+}
+
+addPlayerInList()
+
+/*
+    WELCOME TO THE STRIVE TEAM GENERATOR!
+
+    In this app you will generate teams and assign the participants to them.
+    You will use the Bootstrap components for the elements of your app.
+
+    The features are the following:
+        The user should be able to add a list of names, find a way to get the user input and fill a list with the new name.
+        The user should be able to set the number of teams he wants, so that they will be generated. A team is just another list to be displayed.
+        An "Assign" button will be required in order to launch the team generation functionality.
+        Every time the user clicks on the "Assign" button, a random name is placed in a team and removed from the initial list.
+        Each team should be inside a "column" to be responsive.
+        [EXTRA] Create a button to remove a user from a team, and putting him back in the main list, waiting to be reassigned.
+        [EXTRA] Create a button to reset the state of the app.
+*/
